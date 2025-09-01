579e687692bfa864b37b0b574102f3224d766882
Billy Och <ochiengbilly02@gmail.com>
2025-09-01 13:03:04 +0300

    Adding the bookshelf app into the project and performing CRUD operations

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z4hdik@cmrd5i%5t)j@uv%&74r7v_t8f#vm=q9kg@2220%$^ny'
+SECRET_KEY = 'django-insecure-%mt)ieb6aindus%)iqe&gh^n_y4w6g!=nuv#j54m22(fwa%(!n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
