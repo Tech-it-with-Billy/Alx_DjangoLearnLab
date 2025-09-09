bc2bac9ef2526035719b47d26706f5b059ea5020
Billy Och <ochiengbilly02@gmail.com>
2025-09-09 15:09:57 +0300

    updating the arguments in query samples file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,10 +5,10 @@ def books_by_author(author):
     author = Author.objects.get(author=author)
     return Book.objects.filter(author=author)    
 
-def books_in_library(library):
-    library = Library.objects.get(name=library)
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
     return library.books.all()
 
-def librarian_of_library(library):
-    library = Library.objects.get(name=library)
+def librarian_of_library(library_name):
+    library = Library.objects.get(name=library_name)
     return library.librarian
