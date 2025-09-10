6362aefabadf13ba56f72377f33dcc4f7db22c80
Billy Och <ochiengbilly02@gmail.com>
2025-09-10 12:32:51 +0300

    Update: Adding Views and URL Configuration to the relationship app

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', views.book_list, name='book_list'),
+    path('library/', views.LibraryView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
+from django.views.generic import DetailView
 from django.shortcuts import render
+from .models import Author, Book, Library, Librarian
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    authors = Author.objects.all()
+    return render(request, 'relationship_app/book_list.html', {'books': books, 'authors': authors})
+
+class LibraryView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()
+        context['books'] = library.books.all()
