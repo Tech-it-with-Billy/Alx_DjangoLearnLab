c8a0c214039571d7280232d5f69d3d3fd63e7fa3
Billy Och <ochiengbilly02@gmail.com>
2025-09-10 15:56:24 +0300

    Updating the login, logout and signup paths in the urls.py file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', list_books, name='list_books'),
     path('library/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', SignUpView.as_view(), name='templates/relationship_app/signup'),
+    path('Login/', LoginView.as_view(), name='templates/relationship_app/login'),
+    path('Logout/', LogoutView.as_view(), name='templates/relationship_app/logout'),
 ]
