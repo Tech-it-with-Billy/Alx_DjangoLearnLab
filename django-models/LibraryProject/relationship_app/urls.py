3c215b82102eaa48745cffb20e68e950478d7d3f
Billy Och <ochiengbilly02@gmail.com>
2025-09-10 20:24:07 +0300

    Updating url paths in the urls.py file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
     path('member-dashboard/', views.member_view, name='member_view'),
     path('books/', views.list_books, name='list_books'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('books/add_book/', views.add_book, name='add_book'),
+    path('books/edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
