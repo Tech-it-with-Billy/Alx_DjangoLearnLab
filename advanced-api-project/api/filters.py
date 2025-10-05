c98a2ee45148aa3d075167b8a15013ad96afef6d
Billy Och <ochiengbilly02@gmail.com>
2025-10-05 20:32:25 +0300

    Implementing Filtering, Searching, and Ordering in Django REST Framework

---
+++ advanced-api-project/api/filters.py
@@ -0,0 +1,15 @@
+import django_filters
+from .models import Book
+
+class BookFilter(django_filters.FilterSet):
+    """
+    Custom filter for Book model.
+    Allows filtering by title, author, and publication_year.
+    """
+    title = django_filters.CharFilter(lookup_expr='icontains')
+    author = django_filters.CharFilter(lookup_expr='icontains')
+    publication_year = django_filters.NumberFilter()
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
